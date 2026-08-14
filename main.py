f019ca9f58a8d0e425c788a9af7bac2cd2be3a38
NotZenith <zenith@example.com>
2026-08-14 15:04:19 +0545

    Step 6: Refactor main.py to use modular architecture

--- main.py
+++ main.py
@@ -1,85 +1,37 @@
 import cv2
-from cvzone.HandTrackingModule import HandDetector
-from time import sleep
-import numpy as np
-import cvzone
-from pynput.keyboard import Controller
+from camera import Camera
+from hand_tracker import HandTracker
+from keyboard.keyboard_manager import KeyboardManager
+from utils.logger import logger
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+def main():
+    logger.info("Starting Virtual Keyboard application...")
+    
+    cam = Camera()
+    tracker = HandTracker()
+    kb_manager = KeyboardManager()
 
-detector = HandDetector(detectionCon=0.8)
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
-finalText = ""
+    while True:
+        img = cam.get_frame()
+        if img is None:
+            break
 
-keyboard = Controller()
+        # Hand detection
+        img = tracker.find_hands(img)
+        lm_list, _ = tracker.get_position(img)
 
+        # Keyboard logic and rendering
+        img = kb_manager.draw_all(img)
+        img = kb_manager.update(img, lm_list, tracker.detector)
 
-def drawAll(img, buttonList):
-    imgNew = np.zeros_like(img, np.uint8)
-    for button in buttonList:
-        x, y = button.pos
-        cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
-                          20, rt=0)
-        cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]),
-                      (255, 0, 255), cv2.FILLED)
-        cv2.putText(imgNew, button.text, (x + 40, y + 60),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+        cv2.imshow("Virtual Keyboard", img)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    out = img.copy()
-    alpha = 0.5
-    mask = imgNew.astype(bool)
-    out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
-    return out
+    cam.release()
+    cv2.destroyAllWindows()
+    logger.info("Application closed.")
 
-
-class Button():
-    def __init__(self, pos, text, size=[85, 85]):
-        self.pos = pos
-        self.size = size
-        self.text = text
-
-
-buttonList = []
-for i in range(len(keys)):
-    for j, key in enumerate(keys[i]):
-        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
-
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList, bboxInfo = detector.findPosition(img)
-    img = drawAll(img, buttonList)
-
-    if lmList:
-        for button in buttonList:
-            x, y = button.pos
-            w, h = button.size
-
-            # Check if index finger tip is over the button
-            if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
-                cv2.rectangle(img, (x - 5, y - 5), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
-                cv2.putText(img, button.text, (x + 20, y + 65),
-                            cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-
-                l, _, _ = detector.findDistance(8, 12, img)
-
-                ## when clicked (index + middle finger close)
-                if l < 30:
-                    keyboard.press(button.text)
-                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
-                    cv2.putText(img, button.text, (x + 20, y + 65),
-                                cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-                    finalText += button.text
-                    sleep(0.15)
-
-    # Text display box
-    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
-    cv2.putText(img, finalText, (60, 430),
-                cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    main()
