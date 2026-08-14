5545236df4fb5fb3a36e1e16d5ab33117b38a529
NotZenith <zenith@example.com>
2026-08-14 15:03:56 +0545

    Step 4: Add Button class and layout config

---
+++ keyboard/button.py
@@ -0,0 +1,7 @@
+import cv2
+
+class Button:
+    def __init__(self, pos, text, size=(85, 85)):
+        self.pos = pos
+        self.text = text
+        self.size = size
---
+++ keyboard/layouts.py
@@ -0,0 +1,3 @@
+import config
+
+QWERTY = config.KEYS
